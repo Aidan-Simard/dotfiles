0ad34013dc1c63d72b2f4579c33f33fa32293a97
Aidan-Simard <aidansimard@yahoo.ca>
2024-05-26 10:41:08 -0400

    add code action

--- lua/aidan/remap.lua
+++ lua/aidan/remap.lua
@@ -5,3 +5,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>er", vim.diagnostic.goto_next, opts)
+
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
